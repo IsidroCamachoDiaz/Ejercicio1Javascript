af28385f762c61a250a2031caf10cd93b8eb2b34
DESKTOP-5AFE429\Isidro Camacho <isidroswicth@gmail.com>
2023-02-13 10:05:52 +0100

    13-2-2023

---
+++ ejercicoJavascript1/src/main/webapp/script/script.js
@@ -0,0 +1,4 @@
+//En esta archivo se hace las funciones para la pagina web
+//Se pone el mensaje donde quiera que enseñe en la pestaña con la funcion console.log
+console.log("Hola Mundo!");
+console.log("Soy el primer Script");
